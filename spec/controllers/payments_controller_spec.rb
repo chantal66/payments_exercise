7985030e5b2a2a4edfec5024d4fc5318169503c0
chantal <cjustamm66@me.com>
2019-03-10 09:41:46 -0600

    adds payments controller spec for #create

---
+++ spec/controllers/payments_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describre PaymentsController, type: :controller do
+  describe '#create' do
+    let(:loan) { Loan.create!(funded_amount: 150.0) }
+    let(:valid_params) { { payment_date: Date.today, payment_amount: 50.0 } }
+
+    it 'responds with a 200' do
+      post :create, params: { payment: valid_params, loan_id: loan.id, format: :json }
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
